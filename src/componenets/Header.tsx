54381f695a5dc4c2e00c5a819919928bfd7a5358
skim817 <skim817@aucklanduni.ac.nz>
2019-08-12 09:08:32 +1200

    Create Header

---
+++ src/componenets/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className='headerText'>Plan for Your Perfect Date</h1>
+        </div>
+    )
+}
